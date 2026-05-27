6da5431a575c5816464348530e49fa5019646627
pppixsos <135617740+pppixsos@users.noreply.github.com>
2026-05-27 11:08:35 -0300

    Update ShieldActionExtension.swift

--- PPPIXShieldAction/ShieldActionExtension.swift
+++ PPPIXShieldAction/ShieldActionExtension.swift
@@ -1,30 +1,26 @@
 import ManagedSettings
+import Foundation
 
 class ShieldActionExtension: ShieldActionDelegate {
 
-    override func handle(action: ShieldAction, for application: Application, completionHandler: @escaping (ShieldActionResponse) -> Void) {
-        switch action {
-        case .primaryButtonPressed:
-            // Abre PPPIX direto na tela de senha
-            if let url = URL(string: "pppix://unlock") {
-                self.open(url)
-            }
-            completionHandler(.defer)
-        case .secondaryButtonPressed:
-            completionHandler(.defer)  // .defer mantém o shield aberto em vez de fechar
-        @unknown default:
-            completionHandler(.defer)
-        }
+    override func handle(action: ShieldAction, for application: ApplicationToken, completionHandler: @escaping (ShieldActionResponse) -> Void) {
+        openUnlockURL(completionHandler: completionHandler)
     }
 
-    override func handle(action: ShieldAction, for webDomain: WebDomain, completionHandler: @escaping (ShieldActionResponse) -> Void) {
-        switch action {
-        case .primaryButtonPressed:
-            if let url = URL(string: "pppix://unlock") {
-                self.open(url)
-            }
+    override func handle(action: ShieldAction, for webDomain: WebDomainToken, completionHandler: @escaping (ShieldActionResponse) -> Void) {
+        openUnlockURL(completionHandler: completionHandler)
+    }
+
+    override func handle(action: ShieldAction, for category: ActivityCategoryToken, completionHandler: @escaping (ShieldActionResponse) -> Void) {
+        openUnlockURL(completionHandler: completionHandler)
+    }
+
+    private func openUnlockURL(completionHandler: @escaping (ShieldActionResponse) -> Void) {
+        guard let url = URL(string: "pppix://unlock") else {
             completionHandler(.defer)
-        default:
+            return
+        }
+        extensionContext?.open(url) { _ in
             completionHandler(.defer)
         }
     }
